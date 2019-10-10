e3cc770dfb684c329157c6d7f4f348d5fc42c2b3
Sugizaki Yukimasa <ysugi@idein.jp>
2019-10-10 18:46:01 +0900

    setup.py: Fix Raspberry Pi version it supports

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
         name = 'py-videocore6',
         packages = ['videocore6'],
         version = '0.0.0',
-        description = 'Python library for GPGPU programming on Raspberry Pi',
+        description = 'Python library for GPGPU programming on Raspberry Pi 4',
         author = 'Sugizaki Yukimasa',
         author_email = 'ysugi@idein.jp',
         ext_modules = cythonize(
